f94a6507a19ab1955cdef67b5b9e575a4bf8d92c
mk <majd.khoury93@gmail.com>
2018-01-27 17:09:18 -0500

    Added new route to make city request API call

--- query_test.py
+++ query_test.py
@@ -2,6 +2,7 @@ import httplib2
 import json
 from flask import Flask, jsonify, request, url_for, abort, g, render_template, redirect
 import requests
+import string
 app = Flask(__name__)
 
 @app.route('/demo')
@@ -9,15 +10,33 @@ def demo():
     output = "<html><body><h1>Hello</h1></body></html>"
     return output
 
+@app.route('/index', methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        hello = "hello"
+        return render_template('build/index.html', hello = hello)
+    else:
+        return null
 
+@app.route('/astuce/<string:cityName>')
+def astuce(cityName):
+    url = ('http://conu.astuce.media/api/cities/query?long_name=%s' % (cityName))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    return jsonify(result)
+    print result
+
+#
+# @app.route('/astuce/<string:cityName>')
 # def getAstuceCity(cityName):
 #     url = ('http://conu.astuce.media/api/cities/query?short_name=%s' % (cityName))
 #     h = httplib2.Http()
 #     result = json.loads(h.request(url,'GET')[1])
-#     template = json.dumps(result)
-#     render_template('index.html', template = template)
 #     return result
-#     # print result
+    #template = json.dumps(result)
+    #render_template('index.html', template = template)
+
+    # print result
 #
 # getAstuceCity('Amsterdam')
 
