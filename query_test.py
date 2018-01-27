322e12a3641b6a4df28c7c614b4b292cd1a3c71e
mk <majd.khoury93@gmail.com>
2018-01-27 14:35:41 -0500

    Added a city querying function

--- query_test.py
+++ query_test.py
@@ -1,11 +1,10 @@
 import httplib2
 import json
 
-def getWeatherGroup():
-    url = ('http://conu.astuce.media/api/weather')
+def getAstuceCity(cityName):
+    url = ('http://conu.astuce.media/api/cities/query?short_name=%s' % (cityName))
     h = httplib2.Http()
     result = json.loads(h.request(url,'GET')[1])
     print result
-    return result
 
-getWeatherGroup()
+getAstuceCity('Amsterdam')
