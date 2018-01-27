710bd4aa7b9b641d83603f889653645f9839b618
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-01-27 17:11:37 -0500

    add

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
+
+  handleClick() {
+    console.log('hey')
+    fetch('/index', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        something: 'hey'
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.handleClick}>team</button>
       </div>
     );
   }
